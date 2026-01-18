6ecfe77fc21f47e66fc6864075fc224895bd78d1
Frederik Tørnstrøm <frederik.toernstroem@gmail.com>
2026-01-18 21:23:01 +0100

    remove notifications faster

--- static/js/standard_methods.js
+++ static/js/standard_methods.js
@@ -39,14 +39,14 @@ function formatDateTime(timestamp) {
 function showToast(message, type = 'info') {
     // Create a simple toast notification
     const toast = document.createElement('div');
-    toast.className = `alert alert-${type} position-fixed top-0 start-30 translate-middle-x mt-3`;
+    toast.className = `alert alert-${type} position-fixed top-0 start-50 translate-middle-x mt-3`;
     toast.style.zIndex = '9999';
     toast.textContent = message;
     document.body.appendChild(toast);
     
     setTimeout(() => {
         toast.remove();
-    }, 3000);
+    }, 500);
 }
 
 /**
